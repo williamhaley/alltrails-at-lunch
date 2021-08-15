80b2d345413bc2081e4de6bae57581c049a89727
Will <willhy@gmail.com>
2021-08-15 11:20:06 -0500

    chore(tooling): basic tooling setup, validation

---
+++ src/Navigation/NavBar.tsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>AllTrails at Lunch</h1>
+      <div><button>Filter</button> <input type="text" placeholder="Search for a restaurant"></input></div>
+    </nav>
+  );
+};
+
+export default NavBar;
