5683a9238daa2582ceebeaad577faf0bc9c0e88e
Will <willhy@gmail.com>
2021-08-18 11:18:48 -0500

    fix: move secret to env (and clean up secret)

--- src/Google/GoogleProvider.tsx
+++ src/Google/GoogleProvider.tsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from 'react';
-import { googleApiKey } from '../config';
+
+const googleApiKey = process.env.REACT_APP_MAPS_KEY;
 
 interface State {
   isGoogleLoaded: boolean;
